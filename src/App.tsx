ecddc8340d8cc2f04cfb1c170b320af8089b4160
vpaesi <paesivitoria@gmail.com>
2025-02-19 20:14:05 -0300

    feat: configuração do frontend #1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-50">
+          <Routes>
+          </Routes>
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
