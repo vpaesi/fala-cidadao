9def0e3b841911e5b13c9ef35436ae7bf0223e74
vpaesi <paesivitoria@gmail.com>
2025-02-19 20:44:45 -0300

    feat: criação da home #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
       <BrowserRouter>
         <div className="min-h-screen bg-gray-50">
           <Routes>
+          <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </BrowserRouter>
