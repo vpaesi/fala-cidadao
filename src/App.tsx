45b20bdf1e2595bdf570994b7873b008984a1f22
vpaesi <paesivitoria@gmail.com>
2025-02-23 13:28:23 -0300

    feat: criação da página de novas denúncias

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { NovaDenuncia } from './pages/NovaDenuncia';
+import { DenunciasProvider } from './context/DenunciasContext';
 
 function App() {
   return (
+    <DenunciasProvider>
       <BrowserRouter>
         <div className="min-h-screen bg-gray-50">
           <Routes>
-          <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/nova-denuncia" element={<NovaDenuncia />} />
           </Routes>
         </div>
       </BrowserRouter>
+    </DenunciasProvider>
   );
 }
 
